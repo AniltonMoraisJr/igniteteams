ccf1e8b05cdbd59f5350fc3e57c442fe83d745c0
AniltonFMoraisJr <anilton.morais@bovexo.com>
2023-06-28 23:11:13 -0300

    First Commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+
+import { Container, Title } from "./styles";
+
+const Groups: React.FC = () => {
+  return (
+    <Container>
+      <Title>Groups</Title>
+    </Container>
+  );
+};
+
+export default Groups;
