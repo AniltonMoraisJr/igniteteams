2358f83b745c51ad88e003333ce651481d0b686c
AniltonFMoraisJr <anilton.morais@bovexo.com>
2023-06-29 00:15:12 -0300

    feature: Creating new component: GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import Header from "@components/Header";
 import { Container } from "./styles";
 import Highlight from "@components/Highlight";
+import GroupCard from "@components/GroupCard";
 
 const Groups: React.FC = () => {
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 };
