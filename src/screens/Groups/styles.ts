2d5acac69465d231765315be1e5a2c4302c34a7b
AniltonFMoraisJr <anilton.morais@bovexo.com>
2023-06-29 23:41:53 -0300

    chore: Refactoring styles using css helper from styled-components library

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -3,9 +3,5 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   flex: 1;
   background-color: ${(props) => props.theme.COLORS.GRAY_700};
-`;
-
-export const Title = styled.Text`
-  font-size: ${({ theme }) => `${theme.FONT_SIZE.SM}px`};
-  color: ${(props) => props.theme.COLORS.WHITE};
+  padding: 24px;
 `;
