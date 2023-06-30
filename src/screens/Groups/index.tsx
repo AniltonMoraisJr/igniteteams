55ce3fb06cd523ad80f00280e0a4a61efe68c14c
AniltonFMoraisJr <anilton.morais@bovexo.com>
2023-06-29 22:57:40 -0300

    feature: Implementing useState and FlatList to render groups´s list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "@components/Header";
 import { Container } from "./styles";
 import Highlight from "@components/Highlight";
 import GroupCard from "@components/GroupCard";
+import { FlatList } from "react-native";
+import ListEmpty from "@components/ListEmpty";
 
 const Groups: React.FC = () => {
+  const [groups, setGroups] = useState([]);
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
-
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   );
 };
