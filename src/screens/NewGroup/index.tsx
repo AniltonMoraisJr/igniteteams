bdbf08687f3345355e06921ef817672a5b37498f
AniltonFMoraisJr <anilton.morais@bovexo.com>
2023-07-05 23:49:36 -0300

    feature: Implementing the transportation of paramns between screens´s NewGroup and Players

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Content, Icon } from "./styles";
 import Header from "@components/Header";
 import Highlight from "@components/Highlight";
@@ -6,9 +6,10 @@ import Button from "@components/Button";
 import Input from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
 const NewGroup: React.FC = () => {
+  const [group, setGroup] = useState("");
   const navigation = useNavigation();
   const handleNew = () => {
-    navigation.navigate("players", { group: "Rocket" });
+    navigation.navigate("players", { group });
   };
   return (
     <Container>
@@ -19,7 +20,11 @@ const NewGroup: React.FC = () => {
           title="Nova turma"
           subtitle="Crie a turma para adicionar as pessoas"
         />
-        <Input placeholder="Nome da turma" />
+        <Input
+          value={group}
+          placeholder="Nome da turma"
+          onChangeText={setGroup}
+        />
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,27 +1,33 @@
 import React, { useState } from "react";
+import { FlatList } from "react-native";
+import { useRoute } from "@react-navigation/native";
+
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
 import ButtonIcon from "@components/ButtonIcon";
 import Filter from "@components/Filter";
 import Header from "@components/Header";
 import Highlight from "@components/Highlight";
 import Input from "@components/Input";
-import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
-import { FlatList } from "react-native";
 import PlayerCard from "@components/PlayerCard";
 import ListEmpty from "@components/ListEmpty";
 import Button from "@components/Button";
+
+type RouteParams = {
+  group: string;
+};
 
 const Players: React.FC = () => {
   const [team, setTeam] = useState("Time A");
   const [players, setPlayers] = useState([]);
 
+  const route = useRoute();
+  const { group } = route.params as RouteParams;
+
   return (
     <Container>
       <Header showBackButton />
-      <Highlight
-        title="Nome da turma"
-        subtitle="Adicione a galera e separe os times"
-      />
+      <Highlight title={group} subtitle="Adicione a galera e separe os times" />
 
       <Form>
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
