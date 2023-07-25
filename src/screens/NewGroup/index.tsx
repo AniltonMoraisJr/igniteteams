453b9ae6fff085fcb9778fff7b2f6b9b4d9036d4
AniltonFMoraisJr <anilton.morais@bovexo.com>
2023-07-25 00:03:16 -0300

    feature: Creating and handling error and exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Alert } from "react-native";
 import { Container, Content, Icon } from "./styles";
 import Header from "@components/Header";
 import Highlight from "@components/Highlight";
@@ -6,15 +7,24 @@ import Button from "@components/Button";
 import Input from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
 const NewGroup: React.FC = () => {
   const [group, setGroup] = useState("");
   const navigation = useNavigation();
   const handleNew = async () => {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma");
+      }
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (error) {
-      console.error(error);
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possível criar novo grupo");
+        console.error(error);
+      }
     }
   };
   return (
